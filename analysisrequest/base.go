2873c10f9f4573b36e440d41def2ef6363136dae
Leonardo Di Donato <leodidonato@gmail.com>
2023-04-12 23:37:57 +0200

    feat(analysisrequest): create base struct (that implements the BasicAnalysisRequest interface)
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

---
+++ analysisrequest/base.go
@@ -0,0 +1,55 @@
+package analysisrequest
+
+import (
+	"encoding/json"
+	"errors"
+)
+
+var (
+	errBaseSnowflakeEmpty = errors.New("missing snowflake ID")
+)
+
+// base is a struct containing the fields common to all the analysis requests.
+//
+// Notice it doesn't implement `AnalysisRequest` interface,
+// but only the `BasicAnalysisRequest` interface.
+type base struct {
+	RequestType Type   `json:"type"`
+	Snowflake   string `json:"snowflake_id"`
+	Priority    uint8  `json:"priority,omitempty"`
+}
+
+func (arb *base) UnmarshalJSON(data []byte) error {
+	type alias base
+	var res alias
+	if err := json.Unmarshal(data, &res); err != nil {
+		return err
+	}
+	*arb = base(res)
+
+	if err := arb.Validate(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (arb base) Validate() error {
+	if len(arb.Snowflake) == 0 {
+		return errBaseSnowflakeEmpty
+	}
+
+	return nil
+}
+
+func (arb base) HasEcosystem() bool {
+	return arb.RequestType.HasEcosystem()
+}
+
+func (arb base) Type() Type {
+	return arb.RequestType
+}
+
+func (arb base) ID() string {
+	return arb.Snowflake
+}
