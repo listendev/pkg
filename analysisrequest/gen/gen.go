fd7323aeadc6f2e03cdd9dabc5e85b5cc846d2dc
Leonardo Di Donato <leodidonato@gmail.com>
2023-04-15 18:47:13 +0200

    feat(analysisrequest/gen): make New() generate different type analysis requests
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- analysisrequest/gen/gen.go
+++ analysisrequest/gen/gen.go
@@ -14,19 +14,43 @@ var pool = sync.Pool{
 	},
 }
 
-func New() *analysisrequest.NPMAnalysisRequest {
+func New() analysisrequest.AnalysisRequest {
 	snowflakeID := rand.String(19)
 	name := rand.String(rand.Range(3, 20))
 	vers := fmt.Sprintf("%d.%d.%d", rand.Range(0, 42), rand.Range(0, 42), rand.Range(0, 42))
 	shasum := rand.String(40)
-	ret := analysisrequest.NewNPMAnalysisRequest(snowflakeID, name, vers, shasum)
+	// Not perturbating these
+	priority := uint8(0)
+	force := false
 
-	return &ret
+	randomType := rand.Range(1, int(analysisrequest.MaxType()))
+	switch analysisrequest.Type(randomType) {
+	case analysisrequest.Nop:
+		break
+
+	case analysisrequest.NPMInstallWhileFalco:
+		ret, _ := analysisrequest.NewNPM(analysisrequest.NPMInstallWhileFalco, snowflakeID, priority, force, name, vers, shasum)
+
+		return ret
+
+	case analysisrequest.NPMTestWhileFalco:
+		ret, _ := analysisrequest.NewNPM(analysisrequest.NPMTestWhileFalco, snowflakeID, priority, force, name, vers, shasum)
+
+		return ret
+
+	case analysisrequest.NPMDepsDev:
+		ret, _ := analysisrequest.NewNPM(analysisrequest.NPMDepsDev, snowflakeID, priority, force, name, vers, shasum)
+
+		return ret
+
+	}
+
+	return analysisrequest.NewNOP(snowflakeID, priority, force)
 }
 
-func Get(reuseProbability int) *analysisrequest.NPMAnalysisRequest {
+func Get(reuseProbability int) analysisrequest.AnalysisRequest {
 	if reuseProbability > rand.Range(0, 100) {
-		r := pool.Get().(*analysisrequest.NPMAnalysisRequest)
+		r := pool.Get().(analysisrequest.AnalysisRequest)
 		// Put it back because Get() removes it from the pool
 		defer pool.Put(r)
 
