3463b326779dfde946c3151362f0f9c9c314fbe3
Leonardo Di Donato <leodidonato@gmail.com>
2023-04-16 00:10:01 +0200

    feat(analysisrequest): create function that composes the results upload path for the analysis requests
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- analysisrequest/path.go
+++ analysisrequest/path.go
@@ -1,11 +1,33 @@
 package analysisrequest
 
 import (
+	"fmt"
 	"path"
+	"strings"
 )
 
-type ResultUploadPath [4]string
+type ResultUploadPath []string
 
 func (r ResultUploadPath) ToS3Key() string {
-	return path.Join(r[:]...)
+	return path.Join(r...)
+}
+
+func ComposeResultUploadPath(a AnalysisRequest) ResultUploadPath {
+	t := a.Type()
+	c := t.Components()
+	filename := c.Collector
+	suffix := strings.Join(c.Actions, ",")
+	if len(suffix) > 0 {
+		filename = fmt.Sprintf("%s(%s)", filename, suffix)
+	}
+	filename += "." + strings.TrimPrefix(c.Format, ".")
+
+	if c.Ecosystem == EcosystemNPM {
+		arn := a.(NPM)
+
+		return ResultUploadPath{c.Ecosystem, arn.Name, arn.Version, arn.Shasum, filename}
+	}
+
+	// Assuming there are no types - other than Nop - without ecosystem
+	return ResultUploadPath{"nop", a.ID(), filename}
 }
