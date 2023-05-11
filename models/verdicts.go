e175290fd8a3f82d2fb0762f06b28e0cbe9e199b
Leonardo Di Donato <leodidonato@gmail.com>
2023-05-11 15:48:50 +0200

    feat(models): create the Validate() method for Verdict instances
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- models/verdicts.go
+++ models/verdicts.go
@@ -3,7 +3,10 @@ package models
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io"
+
+	"github.com/garnet-org/pkg/validate"
 )
 
 const (
@@ -11,9 +14,42 @@ const (
 	NPMPackageVersionMetadataKey = "npm_package_version"
 )
 
+func (o *Verdict) Validate() error {
+	errors := validate.Validate(o)
+	if errors != nil {
+		ret := "validation error"
+		if len(errors) > 1 {
+			ret += "s"
+		}
+		ret += ": "
+		for i, e := range errors {
+			if i > 0 {
+				ret += "; "
+			}
+			ret += e.Error()
+		}
+
+		return fmt.Errorf(ret)
+	}
+
+	return nil
+}
+
 type Verdicts []Verdict
 
-func (v Verdicts) ToBuffer() (io.Reader, error) {
+func FromBuffer(stream io.Reader) (Verdicts, error) {
+	b := new(bytes.Buffer)
+	b.ReadFrom(stream)
+
+	res := []Verdict{}
+	if err := json.Unmarshal(b.Bytes(), &res); err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+func (v *Verdicts) Buffer() (io.Reader, error) {
 	buf, err := json.Marshal(v)
 	if err != nil {
 		return nil, err
