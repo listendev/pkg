dc84bb599df3b83c6ae766f8c4b7578abe6dbef8
Leonardo Di Donato <leodidonato@gmail.com>
2023-05-17 02:31:30 +0200

    feat(validate): create custom validators for analysis request types and for verdict codes
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- validate/validate.go
+++ validate/validate.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"reflect"
 
+	"github.com/garnet-org/pkg/analysisrequest"
+	"github.com/garnet-org/pkg/verdictcode"
 	en "github.com/go-playground/locales/en"
 	ut "github.com/go-playground/universal-translator"
 	"github.com/go-playground/validator/v10"
@@ -37,6 +39,28 @@ func init() {
 	Singleton.RegisterAlias("severity", "eq_ignore_case=low|eq_ignore_case=medium|eq_ignore_case=high")
 	Singleton.RegisterAlias("amqp", "startswith=amqp://|startswith=amqps://")
 	Singleton.RegisterAlias("store", "startswith=file:///|startswith=s3://")
+	Singleton.RegisterValidation("is_analysisrequest_type", func(fl validator.FieldLevel) bool {
+		f := fl.Field()
+
+		if f.Kind() == reflect.String {
+			_, err := analysisrequest.ToType(f.String())
+
+			return err == nil
+		}
+
+		panic(fmt.Sprintf("bad field type: %T", f.Interface()))
+	})
+	Singleton.RegisterValidation("is_verdictcode", func(fl validator.FieldLevel) bool {
+		f := fl.Field()
+
+		if f.Kind() == reflect.Uint64 {
+			_, err := verdictcode.FromUint64(f.Uint(), false)
+
+			return err == nil
+		}
+
+		panic(fmt.Sprintf("bad field type: %T", f.Interface()))
+	})
 
 	eng := en.New()
 	Translator, _ = (ut.New(eng, eng)).GetTranslator("en")
@@ -73,7 +97,7 @@ func init() {
 	); err != nil {
 		panic(err)
 	}
-	
+
 	if err := Singleton.RegisterTranslation(
 		"amqp",
 		Translator,
@@ -88,7 +112,7 @@ func init() {
 	); err != nil {
 		panic(err)
 	}
-	
+
 	if err := Singleton.RegisterTranslation(
 		"store",
 		Translator,
@@ -97,6 +121,36 @@ func init() {
 		},
 		func(ut ut.Translator, fe validator.FieldError) string {
 			t, _ := ut.T("store", fe.Field())
+
+			return t
+		},
+	); err != nil {
+		panic(err)
+	}
+
+	if err := Singleton.RegisterTranslation(
+		"is_analysisrequest_type",
+		Translator,
+		func(ut ut.Translator) error {
+			return ut.Add("is_analysisrequest_type", "{0} is not a valid analysis request type", true)
+		},
+		func(ut ut.Translator, fe validator.FieldError) string {
+			t, _ := ut.T("is_analysisrequest_type", fe.Field())
+
+			return t
+		},
+	); err != nil {
+		panic(err)
+	}
+
+	if err := Singleton.RegisterTranslation(
+		"is_verdictcode",
+		Translator,
+		func(ut ut.Translator) error {
+			return ut.Add("is_verdictcode", "{0} is not a valid verdict code", true)
+		},
+		func(ut ut.Translator, fe validator.FieldError) string {
+			t, _ := ut.T("is_verdictcode", fe.Field())
 
 			return t
 		},
