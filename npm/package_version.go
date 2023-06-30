cc391424c58e4f7c6b7e93b0aaeedd05a1769e0b
Leonardo Di Donato <leodidonato@gmail.com>
2023-06-30 18:07:26 +0200

    feat(npm): add scripts, dependencies, and devdependencies field to PackageVersion struct
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- npm/package_version.go
+++ npm/package_version.go
@@ -5,10 +5,14 @@ type PackageMaintainer struct {
 	Mail string `json:"email"`
 }
 
+// PackageVersion represents the NPM registry response for the route <package_name>/<version>.
 type PackageVersion struct {
-	Name        string              `json:"name"`
-	Description string              `json:"description"`
-	Version     string              `json:"version"`
-	Dist        Dist                `json:"dist"`
-	Maintainers []PackageMaintainer `json:"maintainers"`
+	Name            string              `json:"name"`
+	Description     string              `json:"description"`
+	Version         string              `json:"version"`
+	Dist            Dist                `json:"dist"`
+	Maintainers     []PackageMaintainer `json:"maintainers"`
+	Scripts         map[string]string   `json:"scripts"`
+	Dependencies    map[string]string   `json:"dependencies"`
+	DevDependencies map[string]string   `json:"devDependencies"`
 }
