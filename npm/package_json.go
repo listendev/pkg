3b36a373a8b215bd994fe4a0ebf4fb84bfa57c7e
Leonardo Di Donato <leodidonato@gmail.com>
2023-06-30 18:16:15 +0200

    feat(npm): introduce functions to read package.json (+ struct)
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

---
+++ npm/package_json.go
@@ -0,0 +1,52 @@
+package npm
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+)
+
+type PackageJSON struct {
+	Name                 string            `json:"name"`
+	Scripts              map[string]string `json:"scripts"`
+	Dependencies         map[string]string `json:"dependencies"`
+	DevDependencies      map[string]string `json:"devDependencies"`
+	PeerDependencies     map[string]string `json:"peerDependencies"`
+	BundleDependencies   []string          `json:"bundleDependencies"`
+	OptionalDependencies map[string]string `json:"optionalDependencies"`
+}
+
+func NewPackageJSONFromDir(dir string) (*PackageJSON, error) {
+	reader, err := readPackageJSON(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	return NewPackageJSONFromReader(reader)
+}
+
+func NewPackageJSONFromReader(reader io.Reader) (*PackageJSON, error) {
+	ret := &PackageJSON{}
+	if err := json.NewDecoder(reader).Decode(ret); err != nil {
+		return nil, fmt.Errorf("couldn't instantiate from the input package.json contents")
+	}
+
+	return ret, nil
+}
+
+func readPackageJSON(dir string) (io.Reader, error) {
+	name := filepath.Join(dir, "package.json")
+
+	f, err := activeFS.Open(name)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil, fmt.Errorf("directory %s does not contain a package.json file", dir)
+		}
+
+		return nil, fmt.Errorf("couldn't read the package.json file")
+	}
+
+	return f, nil
+}
