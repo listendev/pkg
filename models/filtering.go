eacd4b2781a54d5224b6cdf7c12ec774feb4082e
Leonardo Di Donato <leodidonato@gmail.com>
2023-08-28 14:24:55 +0200

    docs(models): document the Filter() function
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- models/filtering.go
+++ models/filtering.go
@@ -6,7 +6,14 @@ import (
 	"encoding/json"
 )
 
-// Filter ...
+// Filter applies a JSONPath expression to the receiving Verdicts and returns the result.
+//
+// See the JSONPath documentation for more information about the syntax:
+// https://goessner.net/articles/JsonPath/index.html#e2.
+//
+// The result is returned as raw (interface{} or interface{} slice) and as a Verdicts instance.
+// Notice that, depending on the JSONPath input, the result may have a different structure than the Verdicts one:
+// in this case this function returns the raw result, a nil Verdicts, and an error due to the conversion failure.
 func (v *Verdicts) Filter(c context.Context, jsonpath string) (interface{}, Verdicts, error) {
 	r, err := v.Buffer()
 	if err != nil {
