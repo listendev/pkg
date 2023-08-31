e9f19dec5b4cbbbfa9c5a19628b93d1481545ff6
Leonardo Di Donato <leodidonato@gmail.com>
2023-08-31 11:53:23 +0200

    docs(models): document the language
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- models/lang.go
+++ models/lang.go
@@ -5,4 +5,29 @@ import (
 	"github.com/PaesslerAG/jsonpath"
 )
 
+// lang is used to evaluate JSONPath expressions.
+//
+// lang is the union of the following languages: base, arithmetic, bitmask, text, propositional logic, ljson languages, and jsonpath prefix extensions. Plus the following operators:
+//
+//	in: a in b is true iff value a is an element of array b.
+//	??: a ?? b returns a if a is not false or nil, otherwise n (colaesce operator).
+//	?: a ? b : c returns b if bool a is true, otherwise b.
+//
+// And the following functions:
+//
+//	date(a): it parses string `a``. Notice `a`` must match RFC3339, ISO8601, ruby date, or unix date.
+//
+// The base language contains equal (==) and not equal (!=), perentheses and general support for variables, constants and functions. It contains true and false, (floating point) number, string  ("" or “) and char (”) constants.
+//
+// The arithmetic language contains base, plus(+), minus(-), divide(/), power(**), negative(-) and numerical order (<=,<,>,>=).
+//
+// The bitmask language contains base, bitwise and(&), bitwise or(|) and bitwise not(^).
+//
+// The text language contains base, lexical order on strings (<=,<,>,>=), regex match (=~) and regex not match (!~).
+//
+// The propositional logic language contains not(!), and (&&), or (||).
+//
+// The ljson language contains json objects ({string:expression,...}) and json arrays ([expression, ...]).
+//
+// The jsonpath extension language adds support for the $ and @ prefix extensions as per JSONPath grammar.
 var lang = gval.Full(jsonpath.Language())
