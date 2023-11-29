30999c29f0f713fb41c81f3a781880534b8ea71e
Leonardo Di Donato <leodidonato@gmail.com>
2023-11-29 21:11:57 +0100

    test(analysisrequest): test also PyPi ecosystem
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- analysisrequest/type_components_test.go
+++ analysisrequest/type_components_test.go
@@ -8,6 +8,12 @@ import (
 )
 
 func TestGetEcosystemFrom(t *testing.T) {
+	_, err0 := ecosystem.FromString("nope")
+	if assert.NotNil(t, err0) {
+		assert.Error(t, err0)
+	}
+
+	// NPM
 	eco1, err1 := ecosystem.FromString("NPM")
 	assert.Nil(t, err1)
 	assert.NotNil(t, eco1)
@@ -18,8 +24,14 @@ func TestGetEcosystemFrom(t *testing.T) {
 
 	assert.Equal(t, eco1, eco2)
 
-	_, err3 := ecosystem.FromString("nope")
-	if assert.NotNil(t, err3) {
-		assert.Error(t, err3)
-	}
+	// PyPi
+	eco3, err3 := ecosystem.FromString("PyPi")
+	assert.Nil(t, err3)
+	assert.NotNil(t, eco3)
+
+	eco4, err4 := ecosystem.FromString("pypi")
+	assert.Nil(t, err4)
+	assert.NotNil(t, eco4)
+
+	assert.Equal(t, eco3, eco4)
 }
