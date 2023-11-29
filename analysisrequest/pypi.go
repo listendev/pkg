92d11542b8faad2cc7ceb56f7d145d21c5056cbb
Leonardo Di Donato <leodidonato@gmail.com>
2023-11-29 21:12:42 +0100

    feat(analysisrequest): create struct representing PyPi analysis requests
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

---
+++ analysisrequest/pypi.go
@@ -0,0 +1,29 @@
+package analysisrequest
+
+import (
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type pypiPackage struct {
+	Name    string `json:"name"`
+	Version string `json:"version,omitempty"`
+	// FIXME: shasum?
+	// Shasum  string `json:"shasum,omitempty"`
+}
+
+type PyPi struct {
+	base
+	pypiPackage
+}
+
+func (arn PyPi) Publishing() (*amqp.Publishing, error) {
+	return ComposeAMQPPublishing(&arn)
+}
+
+func (arn PyPi) ResultsPath() ResultUploadPath {
+	return ComposeResultUploadPath(&arn)
+}
+
+func (arn PyPi) String() string {
+	return arn.Name + "@" + arn.Version + "(" + arn.Type().String() + ")"
+}
