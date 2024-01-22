8a9df02c422d5d38404417552474c3228c08e506
Leonardo Di Donato <leodidonato@gmail.com>
2024-01-22 16:03:19 +0100

    feat(npm): introduce LatestVersionTime() method
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- npm/package_list.go
+++ npm/package_list.go
@@ -1,6 +1,9 @@
 package npm
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 type VersionTime map[string]time.Time
 
@@ -8,4 +11,14 @@ type PackageList struct {
 	Name     string                    `json:"name"`
 	Versions map[string]PackageVersion `json:"versions"`
 	DistTags DistTags                  `json:"dist-tags"`
+	Time     map[string]time.Time      `json:"time"`
+}
+
+func (l *PackageList) LatestVersionTime() (*time.Time, error) {
+	t, ok := l.Time[l.DistTags.Latest]
+	if !ok {
+		return nil, fmt.Errorf("couldn't find the release time of the latest version")
+	}
+
+	return &t, nil
 }
