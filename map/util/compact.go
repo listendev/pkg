78c1685c7c8140cbd5f23a26ad472ce5fd8dd735
Leonardo Di Donato <leodidonato@gmail.com>
2024-02-20 12:20:30 +0100

    feat(map/util): create utility to compact a map by deleting all the keys (also recursively) with empty values
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

---
+++ map/util/compact.go
@@ -0,0 +1,28 @@
+package maputil
+
+import (
+	stockmap "github.com/ghetzel/go-stockutil/maputil"
+	typeutil "github.com/listendev/pkg/type/util"
+)
+
+func Compact(input map[string]interface{}) (map[string]interface{}, error) {
+	var output = make(map[string]interface{})
+
+	if err := stockmap.Walk(input, func(value interface{}, path []string, isLeaf bool) error {
+		if !typeutil.IsEmpty(value) {
+			if typeutil.IsArray(value) {
+				stockmap.DeepSet(output, path, value)
+
+				return stockmap.SkipDescendants
+			} else if isLeaf {
+				stockmap.DeepSet(output, path, value)
+			}
+		}
+
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+
+	return output, nil
+}
