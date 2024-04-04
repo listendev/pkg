5fe89530528fcaaba6521e5dcc41b967287383ff
Leonardo Di Donato <leodidonato@gmail.com>
2024-04-04 23:53:59 +0200

    feat(lockfile): support multiple lockfiles of the same type but dedupe duplicate paths
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- lockfile/funcs.go
+++ lockfile/funcs.go
@@ -3,10 +3,14 @@ package lockfile
 import (
 	"fmt"
 	"path/filepath"
+	"regexp"
+	"strconv"
 	"strings"
 
 	"github.com/XANi/goneric"
+	"github.com/go-playground/validator/v10"
 	"github.com/listendev/pkg/ecosystem"
+	typeutil "github.com/listendev/pkg/type/util"
 	"github.com/listendev/pkg/validate"
 )
 
@@ -57,34 +61,65 @@ func FromEcosystem(eco ecosystem.Ecosystem) []Lockfile {
 }
 
 // Map maps the given paths to the supported lockfiles.
-func Map(paths []string) map[Lockfile]string {
-	lockfiles := goneric.SliceMapSkip(func(f string) (Lockfile, bool) {
+func Map(paths []string) map[Lockfile][]string {
+	ret := map[Lockfile][]string{}
+	for _, f := range paths {
 		l, e := FromPath(f)
 		if e != nil {
-			return None, true
+			continue
 		}
 
-		return l, false
-	}, paths)
+		if _, ok := ret[l]; !ok {
+			ret[l] = []string{f}
+		} else {
+			if !goneric.SliceIn(ret[l], f) {
+				ret[l] = append(ret[l], f)
+			}
+		}
+	}
 
-	return lockfiles
+	return ret
 }
 
 // Existing splits the given maths into two maps:
 // one containing the supported and existing lockfiles,
 // the other containing the remainings.
-func Existing(paths []string) (map[Lockfile]string, map[Lockfile]error) {
-	errorsMap := map[Lockfile]error{}
+func Existing(paths []string) (map[Lockfile][]string, map[Lockfile][]error) {
+	errorsMap := map[Lockfile][]error{}
 	lockfilesMap := Map(paths)
 
 	err := validate.Singleton.Var(lockfilesMap, "filevalue")
 	if err != nil {
 		for _, e := range err.(validate.ValidationErrors) {
-			field := Lockfile(strings.TrimRight(strings.TrimLeft(e.Field(), "["), "]"))
-			errorsMap[field] = fmt.Errorf(e.Translate(validate.Translator))
-			delete(lockfilesMap, field)
+			field, idx := fromFieldError(e)
+			// spew.Dump(field.String())
+			messageErr := fmt.Errorf(e.Translate(validate.Translator))
+			if _, ok := errorsMap[field]; !ok {
+				errorsMap[field] = []error{messageErr}
+			} else {
+				errorsMap[field] = append(errorsMap[field], messageErr)
+			}
+			if len(lockfilesMap[field]) < 2 {
+				delete(lockfilesMap, field)
+			} else {
+				lockfilesMap[field] = typeutil.RemoveFromSliceAt(lockfilesMap[field], idx)
+			}
 		}
 	}
 
 	return lockfilesMap, errorsMap
+}
+
+func fromFieldError(e validator.FieldError) (Lockfile, int) {
+	re := regexp.MustCompile(`\[(?P<field>.*?)\]\[(?P<index>\d+)\]`)
+	m := re.FindStringSubmatch(e.Field())
+	tmp := make(map[string]string)
+	for i, group := range re.SubexpNames() {
+		if i != 0 && group != "" {
+			tmp[group] = m[i]
+		}
+	}
+	idx, _ := strconv.Atoi(tmp["index"])
+
+	return Lockfile(tmp["field"]), idx
 }
