62dce2328d1502e4e59029e5bf0618b3a4be9403
Leonardo Di Donato <leodidonato@gmail.com>
2024-04-04 23:52:54 +0200

    feat: generic utility to remove an element from a slice
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- type/util/array.go
+++ type/util/array.go
@@ -14,3 +14,10 @@ var SliceTypes = []reflect.Kind{
 	reflect.Slice,
 	reflect.Array,
 }
+
+// RemoveFromSliceAt deletes an element at position `at` from the slice.
+//
+// Order matters.
+func RemoveFromSliceAt[T any](slice []T, at int) []T {
+	return append(slice[:at], slice[at+1:]...)
+}
