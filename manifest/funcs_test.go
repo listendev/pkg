df7f7460935e66d96ce7fa2b1cacbb32c54fec96
Leonardo Di Donato <leodidonato@gmail.com>
2024-04-04 23:52:20 +0200

    test(manifest): adapt tests to multiple manifests of the same type
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- manifest/funcs_test.go
+++ manifest/funcs_test.go
@@ -10,41 +10,41 @@ import (
 func TestMap(t *testing.T) {
 	type testCase struct {
 		input []string
-		want  map[Manifest]string
+		want  map[Manifest][]string
 	}
 
 	cases := []testCase{
 		{
-			want: map[Manifest]string{},
+			want: map[Manifest][]string{},
 		},
 		{
 			input: []string{"unknown.json"},
-			want:  map[Manifest]string{},
+			want:  map[Manifest][]string{},
 		},
 		{
 			input: []string{"package.json"},
-			want:  map[Manifest]string{PackageJSON: "package.json"},
+			want:  map[Manifest][]string{PackageJSON: {"package.json"}},
 		},
 		{
 			input: []string{"package.JSON"},
-			want:  map[Manifest]string{PackageJSON: "package.JSON"},
+			want:  map[Manifest][]string{PackageJSON: {"package.JSON"}},
 		},
 		{
 			input: []string{"working/dir/package.JSON"},
-			want:  map[Manifest]string{PackageJSON: "working/dir/package.JSON"},
+			want:  map[Manifest][]string{PackageJSON: {"working/dir/package.JSON"}},
 		},
 		// TODO: uncomment when available
 		// {
 		// 	input: []string{"requirements.txt"},
-		// 	want:  map[Manifest]string{RequirementsTxt: "requirements.txt"},
+		// 	want:  map[Manifest][]string{RequirementsTxt: {"requirements.txt"}},
 		// },
 		// {
 		// 	input: []string{"somedir/requirements.txt"},
-		// 	want:  map[Manifest]string{RequirementsTxt: "somedir/requirements.txt"},
+		// 	want:  map[Manifest][]string{RequirementsTxt: {"somedir/requirements.txt"}},
 		// },
 		// {
 		// 	input: []string{"somedir/requirements.txt", "package.json"},
-		// 	want:  map[Manifest]string{RequirementsTxt: "somedir/requirements.txt", PackageJSON: "package.json"},
+		// 	want:  map[Manifest][]string{RequirementsTxt: {"somedir/requirements.txt"}, PackageJSON: {"package.json"}},
 		// },
 	}
 
@@ -56,52 +56,55 @@ func TestMap(t *testing.T) {
 func TestExisting(t *testing.T) {
 	type testCase struct {
 		input   []string
-		want    map[Manifest]string
-		wantErr map[Manifest]error
+		want    map[Manifest][]string
+		wantErr map[Manifest][]error
 	}
 
 	cases := []testCase{
 		{
-			want:    map[Manifest]string{},
-			wantErr: map[Manifest]error{},
+			want:    map[Manifest][]string{},
+			wantErr: map[Manifest][]error{},
 		},
 		{
 			input:   []string{"unknown.json"},
-			want:    map[Manifest]string{},
-			wantErr: map[Manifest]error{},
+			want:    map[Manifest][]string{},
+			wantErr: map[Manifest][]error{},
 		},
-		// FIXME: doesn't work in GitHub actions?!
-		// {
-		// 	input:   []string{"testdata/package.JSON"},
-		// 	want:    map[Manifest]string{PackageJSON: "testdata/package.JSON"},
-		// 	wantErr: map[Manifest]error{},
-		// },
 		{
 			input:   []string{"package.json"},
-			want:    map[Manifest]string{},
-			wantErr: map[Manifest]error{PackageJSON: fmt.Errorf("package.json not found")},
+			want:    map[Manifest][]string{},
+			wantErr: map[Manifest][]error{PackageJSON: {fmt.Errorf("package.json not found")}},
+		},
+		{
+			input:   []string{"testdata/package.json", "testdata/package.json"},
+			want:    map[Manifest][]string{PackageJSON: {"testdata/package.json"}},
+			wantErr: map[Manifest][]error{},
+		},
+		{
+			input:   []string{"testdata/1/package.json", "testdata/package.json"},
+			want:    map[Manifest][]string{PackageJSON: {"testdata/1/package.json", "testdata/package.json"}},
+			wantErr: map[Manifest][]error{},
 		},
 		// TODO: uncomment when available
 		// {
 		// 	input:   []string{"somedir/requirements.txt"},
-		// 	want:    map[Manifest]string{},
-		// 	wantErr: map[Manifest]error{RequirementsTxt: fmt.Errorf("somedir/requirements.txt not found")},
+		// 	want:    map[Manifest][]string{},
+		// 	wantErr: map[Manifest][]error{RequirementsTxt: fmt.Errorf("somedir/requirements.txt not found")},
 		// },
 		// {
 		// 	input:   []string{"testdata/poetry.lock", "testdata/package.json"},
-		// 	want:    map[Manifest]string{RequirementsTxt: "testdata/poetry.lock", PackageJSON: "testdata/package.json"},
-		// 	wantErr: map[Manifest]error{},
+		// 	want:    map[Manifest][]string{RequirementsTxt: "testdata/poetry.lock", PackageJSON: "testdata/package.json"},
+		// 	wantErr: map[Manifest][]error{},
 		// },
 		// {
 		// 	input:   []string{"unk/requirements.txt", "testdata/package.json"},
-		// 	want:    map[Manifest]string{PackageJSON: "testdata/package.json"},
-		// 	wantErr: map[Manifest]error{RequirementsTxt: fmt.Errorf("unk/requirements.txt not found")},
+		// 	want:    map[Manifest][]string{PackageJSON: "testdata/package.json"},
+		// 	wantErr: map[Manifest][]error{RequirementsTxt: fmt.Errorf("unk/requirements.txt not found")},
 		// },
 		// {
-		// 	// Order matters: the last poetry.lock overrides the previous one
 		// 	input:   []string{"testdata/package.json", "testdata/requirements.txt", "unk/requirements.txt"},
-		// 	want:    map[Manifest]string{PackageJSON: "testdata/package.json"},
-		// 	wantErr: map[Manifest]error{RequirementsTxt: fmt.Errorf("unk/requirements.txt not found")},
+		// 	want:    map[Manifest][]string{PackageJSON: "testdata/package.json"},
+		// 	wantErr: map[Manifest][]error{RequirementsTxt: fmt.Errorf("unk/requirements.txt not found")},
 		// },
 	}
 
