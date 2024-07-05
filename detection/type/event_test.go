8beeb8ba503e7a704cfd83270f6add4390cf4642
Leonardo Di Donato <leodidonato@gmail.com>
2024-07-05 13:18:16 +0200

    test(detection/type): test (un)marshal (JSON, text) functions
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- detection/type/event_test.go
+++ detection/type/event_test.go
@@ -1,6 +1,7 @@
 package detectiontype
 
 import (
+	"encoding/json"
 	"fmt"
 	"math"
 	"strings"
@@ -52,4 +53,52 @@ func TestFromString(t *testing.T) {
 	require.Error(t, e1)
 	_, e2 := FromString("None")
 	require.Error(t, e2)
+}
+
+func TestMarshal(t *testing.T) {
+	for _, c := range events {
+		r, e := json.Marshal(c)
+		assert.Nil(t, e)
+		assert.NotNil(t, r)
+		assert.Equal(t, fmt.Sprintf("%q", c.Case()), string(r))
+	}
+
+	_, err1 := json.Marshal(Event(math.MaxUint64))
+	assert.Error(t, err1)
+
+	_, err2 := json.Marshal(None)
+	assert.Error(t, err2)
+}
+
+func TestUnmarshal(t *testing.T) {
+	for _, x := range events {
+		var v1 Event
+		e1 := json.Unmarshal([]byte(fmt.Sprintf("%q", x.String())), &v1)
+		if assert.Nil(t, e1) {
+			assert.Equal(t, x, v1)
+		}
+
+		var v2 Event
+		e2 := json.Unmarshal([]byte(fmt.Sprintf("%q", x.Case())), &v2)
+		if assert.Nil(t, e2) {
+			assert.Equal(t, x, v2)
+		}
+	}
+
+	var e Event
+	require.Error(t, json.Unmarshal([]byte(fmt.Sprintf("%q", "none")), &e))
+}
+
+func TestUnmarshalText(t *testing.T) {
+	for _, x := range events {
+		var evt1 Event
+		err1 := evt1.UnmarshalText([]byte(x.String()))
+		require.Nil(t, err1)
+		require.Equal(t, evt1, x)
+
+		var evt2 Event
+		err2 := evt2.UnmarshalText([]byte(x.Case()))
+		require.Nil(t, err2)
+		require.Equal(t, evt2, x)
+	}
 }
