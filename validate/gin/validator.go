7283edd45e7e7633a44459375e7ef841ee05b4e9
Leonardo Di Donato <leodidonato@gmail.com>
2024-07-15 19:14:23 +0200

    feat(validate/gin): introduce a gin validator proxy that uses our validator instance underneath
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

---
+++ validate/gin/validator.go
@@ -0,0 +1,86 @@
+package ginval
+
+import (
+	"reflect"
+	"sync"
+
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
+	"github.com/listendev/pkg/validate"
+)
+
+type Validator struct {
+	once     sync.Once
+	validate *validator.Validate
+}
+
+func New() *Validator {
+	return &Validator{
+		validate: validate.Singleton,
+	}
+}
+
+func (v *Validator) lazyinit() {
+	v.once.Do(func() {
+		v.validate = validate.Singleton
+	})
+}
+
+func (v *Validator) Engine() any {
+	v.lazyinit()
+
+	return v.validate
+}
+
+func (v *Validator) validateStruct(obj any) error {
+	v.lazyinit()
+
+	return v.validate.Struct(obj)
+}
+
+func (v *Validator) ValidateStruct(obj any) error {
+	if obj == nil {
+		return nil
+	}
+
+	value := reflect.ValueOf(obj)
+	switch value.Kind() {
+	case reflect.Ptr:
+		return v.ValidateStruct(value.Elem().Interface())
+	case reflect.Struct:
+		return eventuallyMany(v.validateStruct(obj))
+	case reflect.Slice, reflect.Array:
+		count := value.Len()
+		validateRet := make(binding.SliceValidationError, 0)
+		for i := 0; i < count; i++ {
+			if err := v.ValidateStruct(value.Index(i).Interface()); err != nil {
+				validateRet = append(validateRet, err)
+			}
+		}
+		if len(validateRet) == 0 {
+			return nil
+		}
+		return validateRet
+	default:
+		return nil
+	}
+}
+
+func eventuallyMany(err error) error {
+	if err == nil {
+		return err
+	}
+
+	validationErrors, _ := err.(validate.ValidationErrors)
+	// TODO: check the input not having `validate.ValidationErrors` type
+	if len(validationErrors) == 0 {
+		return nil
+	}
+
+	ret := make(binding.SliceValidationError, 0)
+	for _, ve := range validationErrors {
+		ret = append(ret, ve)
+	}
+
+	return ret
+}
