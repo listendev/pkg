81ba4680e36719e7f0e29cd885b95985ff46186a
Leonardo Di Donato <leodidonato@gmail.com>
2024-09-04 17:45:27 +0200

    test(validate): evolve tests for is_detection_event_type custom validator
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- validate/isdetectiontype_test.go
+++ validate/isdetectiontype_test.go
@@ -7,21 +7,20 @@ import (
 	detectiontype "github.com/listendev/pkg/detection/type"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"golang.org/x/exp/maps"
 )
 
-type test struct {
+type isDetectionTypetest struct {
 	AsEnum      detectiontype.Event `validate:"is_detection_event_type=enum"`
 	AsSnakeCase string              `validate:"is_detection_event_type=case"`
 	AsCamelCase string              `validate:"is_detection_event_type"`
 }
 
-type badTypeTest struct {
+type badIsDetectionTypeTest struct {
 	Wrong []uint8 `validate:"is_detection_event_type"`
 }
 
-func TestBadFieldType(t *testing.T) {
-	input := badTypeTest{
+func TestIsDetectionTypeValidatorBadFieldType(t *testing.T) {
+	input := badIsDetectionTypeTest{
 		Wrong: []uint8("pam_config_modification"),
 	}
 	require.PanicsWithValue(t, "bad field type: []uint8", func() {
@@ -31,66 +30,87 @@ func TestBadFieldType(t *testing.T) {
 }
 
 func TestIsDetectionTypeValidator(t *testing.T) {
-	allValid := test{
+	allValid := isDetectionTypetest{
 		AsEnum:      detectiontype.CapabilitiesModification,
 		AsSnakeCase: "capabilities_modification",
 		AsCamelCase: "CapabilitiesModification",
 	}
 	require.NoError(t, Singleton.Struct(allValid))
 
-	invalid := test{
+	invalid := isDetectionTypetest{
 		AsEnum:      detectiontype.ContainerEscapeAttempt,
 		AsSnakeCase: "xyz_escape",
 		AsCamelCase: "XYZEscape",
 	}
-	fieldsWithError := []string{
-		"AsSnakeCase",
-		"AsCamelCase",
+	fieldsWithError := map[string]bool{
+		"AsSnakeCase": true,
+		"AsCamelCase": true,
 	}
 	errors := Singleton.Struct(invalid).(ValidationErrors)
-	fields := map[string]string{}
+	fields := map[string]bool{}
 	for _, err := range errors {
 		errMsg := err.Translate(Translator)
 		assert.True(t, strings.HasSuffix(errMsg, "a valid detection event type"))
-		fields[err.Field()] = errMsg
+		fields[err.Field()] = true
 	}
-	require.Equal(t, fieldsWithError, maps.Keys(fields))
+	require.Equal(t, fieldsWithError, fields)
+}
+
+func TestNoneIsNotAValidDetectionType(t *testing.T) {
+	invalid := isDetectionTypetest{
+		AsEnum:      detectiontype.None,
+		AsSnakeCase: "none",
+		AsCamelCase: "None",
+	}
+	fieldsWithError := map[string]bool{
+		"AsEnum":      true,
+		"AsSnakeCase": true,
+		"AsCamelCase": true,
+	}
+	errors := Singleton.Struct(invalid).(ValidationErrors)
+	fields := map[string]bool{}
+	for _, err := range errors {
+		errMsg := err.Translate(Translator)
+		assert.True(t, strings.HasSuffix(errMsg, "a valid detection event type"))
+		fields[err.Field()] = true
+	}
+	require.Equal(t, fieldsWithError, fields)
 }
 
 func TestIsDetectionTypeValidatorDetectingWrongStringRepresentation(t *testing.T) {
-	invalid := test{
-		AsEnum:      detectiontype.Informational,
-		AsSnakeCase: "informational",
+	invalid := isDetectionTypetest{
+		AsEnum:      detectiontype.ProcessFingerprint,
+		AsSnakeCase: "process_fingerprint",
 		AsCamelCase: "AaaBbb", // String representation
 	}
-	fieldsWithError := []string{
-		"AsCamelCase",
+	fieldsWithError := map[string]bool{
+		"AsCamelCase": true,
 	}
 	errors := Singleton.Struct(invalid).(ValidationErrors)
-	fields := map[string]string{}
+	fields := map[string]bool{}
 	for _, err := range errors {
 		errMsg := err.Translate(Translator)
 		assert.True(t, strings.HasSuffix(errMsg, "a valid detection event type"))
-		fields[err.Field()] = errMsg
+		fields[err.Field()] = true
 	}
-	require.Equal(t, fieldsWithError, maps.Keys(fields))
+	require.Equal(t, fieldsWithError, fields)
 }
 
 func TestIsDetectionTypeValidatorDetectingWrongCaseRepresentation(t *testing.T) {
-	invalid := test{
+	invalid := isDetectionTypetest{
 		AsEnum:      detectiontype.OsNetworkFingerprint,
 		AsSnakeCase: "osnetwork_fingerprint",
 		AsCamelCase: "OsNetworkFingerprint",
 	}
-	fieldsWithError := []string{
-		"AsSnakeCase",
+	fieldsWithError := map[string]bool{
+		"AsSnakeCase": true,
 	}
 	errors := Singleton.Struct(invalid).(ValidationErrors)
-	fields := map[string]string{}
+	fields := map[string]bool{}
 	for _, err := range errors {
 		errMsg := err.Translate(Translator)
 		assert.True(t, strings.HasSuffix(errMsg, "a valid detection event type"))
-		fields[err.Field()] = errMsg
+		fields[err.Field()] = true
 	}
-	require.Equal(t, fieldsWithError, maps.Keys(fields))
+	require.Equal(t, fieldsWithError, fields)
 }
