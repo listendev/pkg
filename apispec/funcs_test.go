28dea3d0469198e1f807d1611460d64094dcca7e
Leonardo Di Donato <leodidonato@gmail.com>
2024-09-25 15:03:41 +0200

    test(apispec): test TokensSlice()
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- apispec/funcs_test.go
+++ apispec/funcs_test.go
@@ -1,6 +1,8 @@
 package apispec
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestTokensMap(t *testing.T) {
 	// Prepare test data
@@ -25,6 +27,78 @@ func TestTokensMap(t *testing.T) {
 
 	// Call the method
 	result := settings.TokensMap()
+
+	// Check if the result matches the expected output
+	if len(result) != len(expected) {
+		t.Errorf("Expected length %d, got %d", len(expected), len(result))
+	}
+
+	for key, expectedValue := range expected {
+		if resultValue, exists := result[key]; !exists || resultValue != expectedValue {
+			t.Errorf("For key %s, expected value %s, got %s", key, expectedValue, resultValue)
+		}
+	}
+}
+
+func TestTokensSlice(t *testing.T) {
+	// Prepare test data
+	key1 := "ABC"
+	key2 := "ZZZ"
+	tokens := map[string]struct {
+		Key *string `json:"key,omitempty"`
+	}{
+		"openai": {Key: &key1},
+		"claude": {Key: &key2},
+	}
+
+	settings := Settings{
+		Tokens: &tokens,
+	}
+
+	// Expected result
+	expected := []string{
+		"CLAUDE_TOKEN=\"ZZZ\"",
+		"OPENAI_TOKEN=\"ABC\"",
+	}
+
+	// Call the method
+	result := settings.TokensSlice()
+
+	// Check if the result matches the expected output
+	if len(result) != len(expected) {
+		t.Errorf("Expected length %d, got %d", len(expected), len(result))
+	}
+
+	for key, expectedValue := range expected {
+		if resultValue := result[key]; resultValue != expectedValue {
+			t.Errorf("Expected value %s, got %s", expectedValue, resultValue)
+		}
+	}
+}
+
+func TestTokensMapWithQuote(t *testing.T) {
+	// Prepare test data
+	key1 := "ABC"
+	key2 := "ZZZ"
+	tokens := map[string]struct {
+		Key *string `json:"key,omitempty"`
+	}{
+		"openai": {Key: &key1},
+		"claude": {Key: &key2},
+	}
+
+	settings := Settings{
+		Tokens: &tokens,
+	}
+
+	// Expected result
+	expected := map[string]string{
+		"OPENAI_TOKEN": "\"ABC\"",
+		"CLAUDE_TOKEN": "\"ZZZ\"",
+	}
+
+	// Call the method
+	result := settings.TokensMap(WithValueDoubleQuotes())
 
 	// Check if the result matches the expected output
 	if len(result) != len(expected) {
